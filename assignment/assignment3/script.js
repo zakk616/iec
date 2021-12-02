e80ad30f5a39d52bb8314a1f64511c47d168e69c
zakk616 <muhammadzakaria616@gmail.com>
2021-12-02 21:23:50 +0500

    added assginment 4

--- assignment/assignment3/script.js
+++ assignment/assignment3/script.js
@@ -3,7 +3,21 @@ function generateTable() {
   if (num > 0) {
     var myString = "";
     for (i = 1; i <= 10; i++) {
-      myString += num + " x " + i + " = " + i * num + "<br/>";
+      myString +=
+        "<div class='row'>" +
+        "<div id='num' class='child'>" +
+        num +
+        "</div>" +
+        "<div id='operator' class='child'> x  </div>" +
+        "<div id='num' class='child'>" +
+        i +
+        "</div>" +
+        "<div id='operator' class='child'> = </div>" +
+        "<div id='num' class='child'>" +
+        i * num +
+        "</div>" +
+        "</div>" +
+        "<br/>";
     }
     document.getElementById("table").innerHTML = myString;
   } else {
