13ff3e09741c998bb6ba2c7eca3586285d42d158
zakk616 <muhammadzakaria616@gmail.com>
2022-02-01 22:44:40 +0500

    js pracrice with constructor

---
+++ practice/script.js
@@ -0,0 +1,26 @@
+var x = 24;
+
+switch (x) {
+  case 20:
+    day = "Sunday";
+    break;
+  case 21:
+    day = "Monday";
+    break;
+  case 22:
+    day = "Tuesday";
+    break;
+  case 23:
+    day = "Wednesday";
+    break;
+  case 24:
+    day = "Thursday";
+    break;
+  case 25:
+    day = "Friday";
+    break;
+  case 26:
+    day = "Saturday";
+}
+
+console.log(day);
